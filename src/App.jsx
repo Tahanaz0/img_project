93f5b95493b2e51d82c62340ddd128ba3f32d5a0
unknown <tahanaz8242@gmail.com>
2023-07-17 20:47:27 +0500

    changeCode

--- src/App.jsx
+++ src/App.jsx
@@ -10,66 +10,11 @@ function App() {
   const [MHover, setMHover] = useState(false);
   const [MouseHandle, setMouseHandle] = useState(false);
 
-  const handleMouseEnter = () => {    setIsHover(true);  };
-
-  const handleMouseLeave = () => {
-    setIsHover(false);
+  const handleMouseEntered = (state) => {
+    state(true);
   };
-
-  const handleMouseEntered = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeaved = () => {
-    setIsHovered(false);
-  };
-
-  const mouseHandle = () => {
-    setHover(true);
-  };
-
-  const mouseEnter = () => {
-    setHover(false);
-  };
-
-  const mouseHandle1 = () => {
-    setHovered(true);
-  };
-
-  const mouseEnter1 = () => {
-    setHovered(false);
-  };
-
-  const mouseHandled = () => {
-    setHove(true);
-  };
-
-  const mouseEntered = () => {
-    setHove(false);
-  };
-
-  const mouseHandleEnter = () => {
-    setMouseHover(true);
-  };
-
-  const mouseHandleLeave = () => {
-    setMouseHover(false);
-  };
-
-  const MouseHandleEnter = () => {
-    setMHover(true);
-  };
-
-  const MouseHandleLeave = () => {
-    setMHover(false);
-  };
-
-  const handlemouse = () => {
-    setMouseHandle(true);
-  };
-
-  const handleMouse = () => {
-    setMouseHandle(false);
+  const handleMouseLeave = (state) => {
+    state(false);
   };
 
   return (
@@ -77,98 +22,63 @@ function App() {
       <div className="decor">
         <div
           className="imgDiv"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          {isHover ? (
-            <img src={require('./img/dimondClr.png')} alt="" className="img img1" />
-          ) : (
-            <img src={require('./img/dimond.webp')} alt="" className="img img1 hover" />
-          )}
+          onMouseEnter={() => handleMouseEntered(setIsHover)}
+          onMouseLeave={() => handleMouseLeave(setIsHover)}>
+             <img src={isHover ? require('./img/dimondClr.png') : require('./img/dimond.webp')} alt="" className="img img1" />
         </div>
 
         <div
           className="imgDiv"
-          onMouseEnter={handleMouseEntered}
-          onMouseLeave={handleMouseLeaved}
+          onMouseEnter={() => handleMouseEntered(setIsHovered)}
+          onMouseLeave={() => handleMouseLeave(setIsHovered)}
         >
-          {isHovered ? (
-            <img src={require('./img/clrjs.jpg')} alt="" className="img img2" />
-          ) : (
-            <img src={require('./img/js logo.jpg')} alt="" className="img img2" />
-          )}
+          <img src={isHovered ? require('./img/clrjs.jpg') : require('./img/js logo.jpg')} alt="" className="img img2" />
+        </div>
+        <div
+          className="imgDiv"
+          onMouseEnter={() => handleMouseEntered(setHover)}
+          onMouseLeave={() => handleMouseLeave(setHover)}
+        >
+          <img src={Hover ? require('./img/clrCpls.png') : require('./img/cplus.png')} alt="" className="img img2" />
         </div>
 
         <div
           className="imgDiv"
-          onMouseEnter={mouseHandle}
-          onMouseLeave={mouseEnter}
+          onMouseEnter={() => handleMouseEntered(setHovered)}
+          onMouseLeave={() => handleMouseLeave(setHovered)}
         >
-          {Hover ? (
-            <img src={require('./img/clrCpls.png')} alt="" className="img img2" />
-          ) : (
-            <img src={require('./img/cplus.png')} alt="" className="img img3" />
-          )}
+          <img src={Hovered ? require('./img/clr react.png') : require('./img/react logo.png')} alt="" className="img img2" />
         </div>
 
         <div
           className="imgDiv"
-          onMouseEnter={mouseHandle1}
-          onMouseLeave={mouseEnter1}
+          onMouseEnter={() => handleMouseEntered(setHove)}
+          onMouseLeave={() => handleMouseLeave(setHove)}
         >
-          {Hovered ? (
-            <img src={require('./img/clr react.png')} alt="" className="img img2" />
-          ) : (
-            <img src={require('./img/react logo.png')} alt="" className="img img4" />
-          )}
+          <img src={Hove ? require('./img/html.png') : require('./img/html5.png')} alt="" className="img img2" />
+        </div>
+        <div
+          className="imgDiv"
+          onMouseEnter={() => handleMouseEntered(setMouseHover)}
+          onMouseLeave={() => handleMouseLeave(setMouseHover)}
+        >
+          <img src={MouseHover ? require('./img/clrpls.jpg') : require('./img/plss.jpg')} alt="" className="img img2" />
         </div>
 
         <div
           className="imgDiv"
-          onMouseEnter={mouseHandled}
-          onMouseLeave={mouseEntered}
+          onMouseEnter={() => handleMouseEntered(setMHover)}
+          onMouseLeave={() => handleMouseLeave(setMHover)}
         >
-          {Hove ? (
-            <img src={require('./img/html.png')} alt="" className="img img2" />
-          ) : (
-            <img src={require('./img/html5.png')} alt="" className="img img5" />
-          )}
+          <img src={MHover ? require('./img/rrclr.jpg') : require('./img/rr.jpg')} alt="" className="img img2" />
         </div>
 
         <div
           className="imgDiv"
-          onMouseEnter={mouseHandleEnter}
-          onMouseLeave={mouseHandleLeave}
+          onMouseEnter={() => handleMouseEntered(setMouseHandle)}
+          onMouseLeave={() => handleMouseLeave(setMouseHandle)}
         >
-          {MouseHover ? (
-            <img src={require('./img/clrpls.jpg')} alt="" className="img img2" />
-          ) : (
-            <img src={require('./img/plss.jpg')} width={90} alt="" className="img img6" />
-          )}
-        </div>
-
-        <div
-          className="imgDiv"
-          onMouseEnter={MouseHandleEnter}
-          onMouseLeave={MouseHandleLeave}
-        >
-          {MHover ? (
-            <img src={require('./img/rrclr.jpg')} alt="" className="img img2" />
-          ) : (
-            <img src={require('./img/rr.jpg')} alt="" className="img img7" />
-          )}
-        </div>
-
-        <div
-          className="imgDiv"
-          onMouseEnter={handlemouse}
-          onMouseLeave={handleMouse}
-        >
-          {MouseHandle ? (
-            <img src={require('./img/dataclr.png')} alt="" className="img img2" />
-          ) : (
-            <img src={require('./img/data.png')} alt="" className="img img8" />
-          )}
+          <img src={MouseHandle ? require('./img/dataclr.png') : require('./img/data.png')} alt="" className="img img2" />
         </div>
       </div>
     </div>
